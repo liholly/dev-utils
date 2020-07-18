5b38e9e196eade65e901a37541bdb3a8c8f9453e
1633203352@qq.com <777871lh>
2020-07-19 00:18:48 +0800

    处理了一些bug

--- src/dom-util/createEl.js
+++ src/dom-util/createEl.js
@@ -1,14 +1,29 @@
 import setAttr from './setAttr.js'
 import setHtml from './setHtml.js'
 
+/**
+ * 创建html元素实例
+ * @param tagName 标签名|html
+ * @param attach
+ * @returns {Element}
+ */
 export default function (tagName, attach) {
-	var _el = document.createElement(tagName);
-	var _k;
+	var _isTag = tagName.length > 10 || (!!tagName.match(/</));
+	var _el = document.createElement(_isTag ? 'div' : tagName);
+
+	//根据传入的tagName类型决定
+	if (_isTag) {
+		setHtml(_el, tagName);
+		_el = (_el.children || [])[0];
+	}
+
+	//设置属性
+	var _k, _i;
 	if (attach) {
 		if (typeof attach === 'string') setHtml(_el, attach);
 		else for (_k in attach) {
-			if (_k === 'style') _el.style[_k] = attach.style[_k];
-			else setAttr(_el, attach[k]);
+			if (_k === 'style') for (_i in attach[_k]) _el.style[_k] = attach[_k][_i];
+			else setAttr(_el, attach[_k]);
 		}
 	}
 	return _el;
--- src/dom-util/removeClass.js
+++ src/dom-util/removeClass.js
@@ -1,4 +1,9 @@
 export default function (el, className) {
-	el.classList.remove(className);
+	if ((typeof el === 'object') && ('length' in el)) {
+		el.forEach(function (item) {
+			item.classList.remove(className)
+		})
+	}
+	else el.classList.remove(className);
 	return el;
 }
